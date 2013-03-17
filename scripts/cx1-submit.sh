10fb19da6f03823a4273b47fb789072d347bd443
Nathan <nathan.chong@gmail.com>
2013-03-17 21:23:01 +0000

    Add scripts for CX1

---
+++ scripts/cx1-submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z ${TAG} ]; then
+  echo "Please give TAG directory, like this"
+  echo "  TAG=<name> $0"
+  exit 1
+fi
+if [ -z ${RUN} ]; then
+  echo "Please give RUN parameter, like this"
+  echo "  RUN=<name> $0"
+  exit 1
+fi
+
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=race_biaccess,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=add-upsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=or-upsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=max-upsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=abstract-upsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=add-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=or-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=max-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=abstract-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=add-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=or-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=blelloch,PATTERN=max-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=race_biaccess,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=add-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=or-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=max-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=abstract-downsweep-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=add-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=or-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=brentkung,PATTERN=max-endspec-*,RUN=${RUN} cx1.sh
+qsub -v TAG=${TAG},TEST=.,PATTERN=hillis-steele,RUN=${RUN} cx1.sh
---
+++ scripts/cx1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=32000mb
+#PBS -l place=free:excl
+
+echo "-------------------------------------------------"
+echo ${TAG}
+echo "-------------------------------------------------"
+cat /proc/cpuinfo
+
+cd /work/nyc04/regression
+export GPUVERIFY_INSTALL_DIR=`pwd`/GPUVerifyInstall
+export PATH=`pwd`/local/bin:$PATH
+export LD_LIBRARY_PATH=`pwd`/local/lib64:`pwd`/local/lib:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=1
+module load python/2.7.3
+
+cd ${PBS_O_WORKDIR}
+cd ${TAG}
+for DIR in `find ${TEST} -maxdepth 1 -name "${PATTERN}" -print`; do
+  echo "-------------------------------------------------"
+  echo ${DIR}
+  cd ${DIR}
+  ${PBS_O_WORKDIR}/submit.py | tee ${RUN}
+  cd -
+done
