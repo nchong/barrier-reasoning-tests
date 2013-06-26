107a39f483c507472784a67dd2cb7ce2568f6d2d
Nathan Chong <nathan.chong@gmail.com>
2013-06-26 17:10:14 +0100

    Sklansky in cuda

---
+++ tests/voigtlaender/sklansky.h
@@ -0,0 +1,20 @@
+template<class O>
+__device__ void scan(char *input, char *output, O op) {
+  __shared__ char result[N];
+
+  unsigned t = threadIdx.x;
+
+  result[2*t] = input[2*t];
+  result[2*t+1] = input[2*t+1];
+
+  for (unsigned d = 1; d < N; d *= 2) {
+    __syncthreads();
+    unsigned block = 2 * (t - (t & (d - 1)));
+    unsigned me    = block + (t & (d - 1)) + d;
+    unsigned spine = block + d - 1;
+    result[me] = op(result[spine], result[me])
+  }
+
+  output[2*t] = result[2*t];
+  output[2*t+1] = result[2*t+1];
+}
